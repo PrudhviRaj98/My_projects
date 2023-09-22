8d6185cc57aa67b87ed7e82cc845114f46ca0ff5
PrudhviRaj98 <41124774+PrudhviRaj98@users.noreply.github.com>
2023-09-22 10:19:58 -0400

    Add files via upload

---
+++ Cube_Project/index.md
@@ -0,0 +1,15 @@
+### Assignment I
+
+Please read the following links:
+
+- https://learn.shayhowe.com/advanced-html-css/transitions-animations/
+
+- https://learn.shayhowe.com/advanced-html-css/transitions-animations/
+
+### Create a web page with the following characteristics:
+
+- Create a 2D and 3D Cube inside the page.
+
+- The Cubes should be animating on hovering.
+
+- Once you are done with the assignment connect with the mentor and get the code reviewed.
---
+++ Cube_Project/script.js
@@ -0,0 +1,18 @@
+var cube = document.querySelector('.cube-3d');
+var radioGroup = document.querySelector('.radio-group');
+
+var currentClass = '';
+
+function changeSide() {
+  var checkedRadio = radioGroup.querySelector(':checked');
+  var showClass = 'show-' + checkedRadio.value;
+  if (currentClass) {
+    cube.classList.remove(currentClass);
+  }
+  cube.classList.add(showClass);
+  currentClass = showClass;
+}
+// set initial side
+changeSide();
+
+radioGroup.addEventListener('change', changeSide);
